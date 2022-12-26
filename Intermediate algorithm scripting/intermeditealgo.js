26f2f850a8c0e43a4232de7f5f07af4f0604f564
Jay Boricha <jayboricha707@gmail.com>
2022-12-26 22:40:57 +0530

    Continued with intermediate algorithm scripting

--- Intermediate algorithm scripting/intermeditealgo.js
+++ Intermediate algorithm scripting/intermeditealgo.js
@@ -78,3 +78,73 @@ function spinalCase(str) {
 
 spinalCase("This Is Spinal Tap");
 //   ----------------------------------------------------------------------------------------------------
+function translatePigLatin(str) {
+  let Regex = /^[^aeiou]+/;
+  let Consonants = str.match(Regex);
+  return Consonants !== null
+    ? str
+        .replace(Regex, "")
+        .concat(Consonants)
+        .concat("ay")
+    : str.concat("way");
+}
+
+translatePigLatin("consonant");
+//   ----------------------------------------------------------------------------------------------------
+function myReplace(str, before, after) {
+  // Find index where before is on string
+  var index = str.indexOf(before);
+  if (str[index] === str[index].toUpperCase()) {
+
+    after = after.charAt(0).toUpperCase() + after.slice(1);
+  } else {
+
+    after = after.charAt(0).toLowerCase() + after.slice(1);
+  }
+
+  str = str.replace(before, after);
+
+  return str;
+}
+
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+//   ----------------------------------------------------------------------------------------------------
+function pairElement(str) {
+
+  const match = function(char) {
+    switch (char) {
+      case "A":
+        return ["A", "T"];
+      case "T":
+        return ["T", "A"];
+      case "C":
+        return ["C", "G"];
+      case "G":
+        return ["G", "C"];
+    }
+  };
+
+  const pairs = [];
+  for (let i = 0; i < str.length; i++) {
+    pairs.push(match(str[i]));
+  }
+
+  return pairs;
+}
+
+pairElement("GCG");
+//   ----------------------------------------------------------------------------------------------------
+function fearNotLetter(str) {
+  for (let i = 0; i < str.length; i++) {
+    const Code = str.charCodeAt(i);
+
+    if (Code !== str.charCodeAt(0) + i) {
+
+      return String.fromCharCode(Code - 1);
+    }
+  }
+  return undefined;
+}
+
+fearNotLetter("abce");
+//   ----------------------------------------------------------------------------------------------------
